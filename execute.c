d3c6c486a058af94d016f9b61b0670c57d3f92f4
youlanjie <ylj18926385312@163.com>
2020-12-22 20:22:06 +0800

    修改源代码

--- execute.c
+++ execute.c
@@ -10,19 +10,27 @@ void edit();
 void help();
 
 int main() {
-	int b=0x31;
-	char a[100];
+	int a=0,b=0x31;
 	while (b != 0x30 || b != 0x1B) {
+		printf("\033[?25l\n");
 		Clear
+		if (a >= 30)
+		{
+			puts("不好意思，您输入的错误次数大于29了，为了防止出错，即将退出程序\033[?25h");
+			return 0;
+		}
 		welcome();
 		b = getchar();
 		getchar();
+		printf("\033[0m\033[?25l\n");
 		Clear
 		switch (b) {
 			case 0x1B:
 			case 0x30:
 			case 0x51:
 			case 0x71:
+				printf("\033[0m\n");
+				Clear
 				return 0;
 				break;
 			case 0x31:
@@ -39,15 +47,17 @@ int main() {
 				break;
 			default:
 				puts("输入错误！即将返回...");
-				system("sleep 0.001");
+				a++;
 				break;
 		}
 	}
+	printf("\033[?25h\n");
+	Clear
 	return 0;
 }
 
 void welcome() {
-	printf("\n\n                                     \033[;31m执行\033[0m\n\n");
+	printf("\n                                     \033[;31m执行\033[0m\n\n\n\n");
 	printf("\033[;33m--------------------------------------------------------------------------------\033[0m\n");
 	printf("\033[;33m|                                                                              |\033[0m\n");
 	printf("\033[;33m|\033[0m                   1.历史文件                    2.新的文件                   \033[;33m|\033[0m\n");
@@ -71,8 +81,8 @@ void edit() {
 }
 
 void help() {
-	puts("选项前面的数字就是触发选项的数字");
-	puts("按下任意按键退出：");
+	puts("\033[34mtip1:选项前面的数字就是触发选项的数字\033[0m\n");
+	puts("\033[33m按下任意按键退出：");
 	getchar();
 	return;
 }
